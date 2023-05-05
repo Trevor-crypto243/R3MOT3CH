1182773c205fac3c7c7e8708d2a5d3f0d4a15411
Trevor Ogina <otitrevor450@gmail.com>
2023-05-05 15:38:36 +0300

    Added sorting of the products

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -10,14 +10,38 @@ use Cart;
 class ShopComponent extends Component
 {
     use WithPagination;
+    public $pageSize = 12;
+    public $orderBy = "Default Sorting";
+
+
+
+
     public function store($product_id,$product_name,$product_price){
         Cart::add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
         session()->flash('success_message','Item addded in cart');
         return redirect()->route('shop.cart');
     }
+
+    public function changePageSize($size){
+        $this->pageSize = $size;
+    }
+
+    public function changeOrderBy($order){
+        $this->orderBy = $order;
+    }
     public function render()
     {
-        $products = Product::paginate(12);
+        if($this->orderBy == 'Price: Low to High' ){
+            $products = product::orderBy('regular_price','ASC')->paginate($this->pageSize);
+        }else  if($this->orderBy == 'Price: Low to High' ){
+            $products = product::orderBy('regular_price','DESC')->paginate($this->pageSize);
+        }else  if($this->orderBy == 'Price: Hight to Lowh' ){
+            $products = product::orderBy('regular_price','ASC')->paginate($this->pageSize);
+        }else  if($this->orderBy == 'Sort by newness' ){
+            $products = product::orderBy('created_at','DESC')->paginate($this->pageSize);
+        }else{
+            $products = Product::paginate($this->pageSize);
+        }
         return view('livewire.shop-component',['products'=>$products]);
     }
 }
