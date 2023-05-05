d264d38a4a1cb6d611779ec7fcdeb68cd18b444a
Trevor Ogina <otitrevor450@gmail.com>
2023-05-05 12:19:32 +0300

    cleared cart successfully

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -20,6 +20,15 @@ class CartComponent extends Component
         Cart::update($rowId,$qty);
 
     }
+    
+    public function destroy($id){
+        Cart::remove($id);
+        session()->flash('success_message','item has been removed');
+    }
+
+    public function clearAll(){
+        Cart::destroy();
+    }
 
 
     public function render()
